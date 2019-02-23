5bdbe536e7030f30aae1d55d36f861ccb474ee65
WILLIAMNGUYEN18 <nguyew2@uw.edu>
2019-02-23 14:43:47 -0800

    CODE implemented Need to test

--- sql/hw3-q10.sql
+++ sql/hw3-q10.sql
@@ -1,3 +1,24 @@
 -- List the rooms that are currently unoccupied at the Hilton.  
 -- 		Example format)
 -- (room #, hotel #) 
+--not in booking?
+--need room, hotel, booking
+--need current time restraint for dateFrom/To
+
+
+SELECT R.roomNo, R.H_hotelNo,
+H.hotelNo, H.hotelName,
+B.H_hotelNo, B.dateFrom, B.dateTo, B.R_roomNo
+FROM ROOM AS R, BOOKING AS B, HOTEL as H 
+WHERE H.hotelNo = R.H_hotelNo AND
+R.H_hotelNo = B.H_hotelNo AND
+R.roomNo = B.R_roomNo AND
+H.hotelName = 'Hilton' AND
+NOT EXISTS (SELECT R.roomNo, R.H_hotelNo,
+H.hotelNo, H.hotelName,
+B.H_hotelNo, B.dateFrom, B.dateTo, B.R_roomNo
+FROM ROOM AS R, BOOKING AS B, HOTEL as H 
+B.dateFrom <= date('now') AND 
+B.dateTo >= date('now') AND
+) 
+
--- sql/hw3-q9.sql
+++ sql/hw3-q9.sql
@@ -1,3 +1,10 @@
 -- Count the number of current bookings at each hotel and display with the hotel name and city. List only the hotels with at least 5 current bookings. 
 --  		Example format)
 -- (# of bookings, hotel name, city)
+--need hotel and booking
+
+SELECT COUNT(BOOKING), H.hotelName, H.city, H.hotelno
+FROM HOTEL AS H, BOOKING
+WHERE H_hotelno  = H.hotelno AND 
+    COUNT(BOOKING) > 5
+GROUP BY HOTEL
